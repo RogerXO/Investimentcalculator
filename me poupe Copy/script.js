6835c8823fb4374ca40b00e152d936886c330f6b
Roger Xavier O <rogerxavier10@gmail.com>
2022-01-31 20:14:25 -0300

    library downloaded and code transformed

--- me poupe Copy/script.js
+++ me poupe Copy/script.js
@@ -1,3 +1,26 @@
+ // configuration
+ const args = {
+    afterFormat(e) { console.log('afterFormat', e); },
+    allowNegative: false,
+    beforeFormat(e) { console.log('beforeFormat', e); },
+    negativeSignAfter: false,
+    prefix: '',
+    suffix: '',
+    fixed: true,
+    fractionDigits: 2,
+    decimalSeparator: ',',
+    thousandsSeparator: '.',
+    cursor: 'move'
+  };
+
+  // Select the element
+  const input1 = SimpleMaskMoney.setMask('#info1', args);
+  // Convert the input value to a number, which you can save e.g. to a database:
+  input1.formatToNumber();
+
+  const input2 = SimpleMaskMoney.setMask('#info2', args);
+  input2.formatToNumber();
+
 /*
 M = Montante
 ic = Initial Capital
@@ -15,14 +38,15 @@ const ninfo4 = document.getElementById('info4')
 const ninfo5 = document.getElementById('info5')
 
 function done() {
-    const ic = Number(ninfo1.value)
-    const pmc = Number(ninfo2.value)
+    const ic = input1.formatToNumber()
+    const pmc = input2.formatToNumber()
     const n = Number(ninfo3.value)
     const i = Number(ninfo4.value)/100
 
     const year_to_month = (Math.pow((1+i), 1.0/12)-1)
-    const i_yearly = (ic *(Math.pow((1+(year_to_month)), n))) + ((pmc *((Math.pow((1+(year_to_month)), n)) - 1))/(year_to_month))
     const i_monthly = (ic *(Math.pow((1+i), n))) + ((pmc *((Math.pow((1+i), n)) - 1))/i)
-
-
+    const i_yearly = (ic *(Math.pow((1+(year_to_month)), n))) + ((pmc *((Math.pow((1+(year_to_month)), n)) - 1))/(year_to_month))
+    
+    //alert(i_monthly)
+    alert(i_yearly)
 }
